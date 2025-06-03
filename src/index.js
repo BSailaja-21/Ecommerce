66c0a8b8d568478a3dfed64cbb06be53262ffadb
BSailaja-21 <bsailaja2127@gmail.com>
2025-06-03 16:00:09 +0530

    beautyproducts

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Nav />
-   
+   npx update-browserslist-db@latest
+
   </React.StrictMode>
 );
 
